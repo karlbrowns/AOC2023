4feaa42af546d9a85c14c98a8511334dfcaf3d26
karlbrowns <simon.k.brrown@gmail.com>
2023-12-01 00:00:53 +0000

    Add project files.

---
+++ TemplateProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCodeUtilities;
+using System.Text.RegularExpressions;
+
+List<string> inputList = AoC.GetInputLines();
+
+void P1()
+{
+    int result = 0;
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int result = 0;
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+P1();
+P2();
