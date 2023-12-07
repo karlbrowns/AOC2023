a054f03ac527b0ab6affb4d8a68677e85a56342b
karlbrowns <simon.k.brrown@gmail.com>
2023-12-07 22:25:31 +0000

    Day 6 done

--- Day_06_Wait_For_It/Program.cs
+++ Day_06_Wait_For_It/Program.cs
@@ -1,27 +1,74 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 
 void P1()
 {
     int result = 0;
     int index = 0;
+    List<int> times = new List<int>();
+    List<int> dists = new List<int>();
     String data = "input.txt";
     foreach (string line in System.IO.File.ReadLines(data))
     {
+        if (index==0)
+        {
+            string[] snums = line.Substring(10).Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in snums)
+            {
+                if (int.TryParse(s, out int o)!=false)
+                {
+                    times.Add(o);
+                }
+            }
+        }
+        if (index == 1)
+        {
+            string[] snums = line.Substring(10).Split( ' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in snums)
+            {
+                if (int.TryParse(s, out int o) != false)
+                {
+                    dists.Add(o);
+                }
+            }
+        }
+        index++;
     }
+    List<int> best = new List<int>();
+    result = 1;
+    for (int i=0; i<times.Count; i++)
+    {
+        int dist = dists[i];
+        int time = times[i];
+        best.Add(0);
+
+        for (int j=0; j<=time; j++)
+        {
+            int k = j * (time - j);
+            if (k > dist) best[i]++;
+        }
+        result *= best[i]; 
+    }
+   
     Console.WriteLine(result);
     Console.ReadLine();
 }
 
 void P2()
 {
-    int result = 0;
-    int index = 0;
-    String data = "input.txt";
-    foreach (string line in System.IO.File.ReadLines(data))
+    int result = 1;
+    long time = 60947882;
+    long dist = 475213810151650;
+    List<int> best = new List<int>();
+    best.Add(0);
+
+    for (long j = 0; j <= time; j++)
     {
+        long k = j * (time - j);
+        if (k > dist) best[0]++;
     }
-    int result = 0;
+    result *= best[0];
     Console.WriteLine(result);
     Console.ReadLine();
 }
---
+++ Day_07_Camel_Cards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+
+void P1()
+{
+    int result = 0;
+    int index = 0;
+    String data = "input.txt";
+    foreach (string line in System.IO.File.ReadLines(data))
+    {
+    }
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int result = 0;
+    int index = 0;
+    String data = "input.txt";
+    foreach (string line in System.IO.File.ReadLines(data))
+    {
+    }
+    int result = 0;
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+P1();
+P2();
