5ebe2cd0680785501eee94a76cda4de681eec0e3
karlbrowns <simon.k.brrown@gmail.com>
2023-12-07 23:24:16 +0000

    Day 7 started - yuck

--- Day_07_Camel_Cards/Program.cs
+++ Day_07_Camel_Cards/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System.Text.RegularExpressions;
 
-
 void P1()
 {
     int result = 0;
     int index = 0;
     String data = "input.txt";
+    List<part1> cards = new List<part1>();
     foreach (string line in System.IO.File.ReadLines(data))
     {
+        string[] parts = line.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+        part1 temp;
+        temp.hand = parts[0];
+        temp.sorted_hand = String.Concat(parts[0].OrderBy(c => c));
+        temp.score = int.Parse(parts[1]);
+        temp.rank = 0;
+        cards.Add(temp);
     }
+    
     Console.WriteLine(result);
     Console.ReadLine();
 }
@@ -21,10 +29,34 @@ void P2()
     foreach (string line in System.IO.File.ReadLines(data))
     {
     }
-    int result = 0;
     Console.WriteLine(result);
     Console.ReadLine();
 }
 
 P1();
 P2();
+
+public struct part1
+{
+    public string hand;
+    public string sorted_hand;
+    public int score;
+    public int rank;
+};
+
+public class tComp : IComparer<part1>
+{
+    public int Compare(part1 x, part1 y)
+    {
+        if (x.sorted_hand)
+
+
+
+
+
+        return 0;
+
+    }
+
+};
+
