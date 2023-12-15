9cc9ede0565de53762fb929f10a177612b9f9071
karlbrowns <simon.k.brrown@gmail.com>
2023-12-15 00:35:34 +0000

    day 7 a little

--- Day_07_Camel_Cards/Program.cs
+++ Day_07_Camel_Cards/Program.cs
@@ -1,4 +1,19 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading;
+using AdventOfCodeUtilities;
+
+/*
+Every hand is exactly one type. From strongest to weakest, they are:
+
+Five of a kind, where all five cards have the same label: AAAAA
+Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+High card, where all cards' labels are distinct: 23456
+*/
 
 void P1()
 {
@@ -16,7 +31,29 @@ void P1()
         temp.rank = 0;
         cards.Add(temp);
     }
-    
+    List<part1> sorted_cards = cards.OrderByLambda(widget => widget, (x , y) =>
+    {
+        char c1, c2;
+        int xcount1 = 0, xcount2 = 0;
+        int ycount1 = 0, ycount2 = 0;
+        c1 = x.sorted_hand[0];
+        xcount1 = x.sorted_hand.Count(xx => xx == c1);
+        if (xcount1 < 5)
+        {
+            c2 = x.sorted_hand[xcount1];
+            xcount2 = x.sorted_hand.Count(xx => xx == c2);
+        }
+        c1 = y.sorted_hand[0];
+        ycount1 = y.sorted_hand.Count(yy => yy == c1);
+        if (ycount1 < 5)
+        {
+            c2 = y.sorted_hand[ycount1];
+            ycount2 = y.sorted_hand.Count(yy => yy == c2);
+        }
+        if (xcount1 > ycount2) return 1;
+        if (ycount1 > xcount1) return -1;
+        return 0;
+    }).ToList();
     Console.WriteLine(result);
     Console.ReadLine();
 }
@@ -42,21 +79,5 @@ public struct part1
     public string sorted_hand;
     public int score;
     public int rank;
-};
-
-public class tComp : IComparer<part1>
-{
-    public int Compare(part1 x, part1 y)
-    {
-        if (x.sorted_hand)
-
-
-
-
-
-        return 0;
-
-    }
-
 };
 
