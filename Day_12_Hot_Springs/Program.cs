95ca308dd5df608b2f61cd569fe2181f5c44f388
karlbrowns <simon.k.brrown@gmail.com>
2023-12-18 21:53:37 +0000

    Day 12 pt 1 done.

--- Day_12_Hot_Springs/Program.cs
+++ Day_12_Hot_Springs/Program.cs
@@ -1,67 +1,95 @@
 ﻿using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Globalization;
+using System.Formats.Asn1;
 
 void P1()
 {
     int result = 0;
     int index = 0;
-    String data = "inputtst.txt";
+    uint[] power2 = new uint[32];
+    uint one = 1;
+    String data = "input.txt";
     List<HotSprings> hotSprings = new List<HotSprings>();
+    
+    
+    for (int i = 0; i < 32; i++) power2[i] = (one<<i) - 1;
     foreach (string line in System.IO.File.ReadLines(data))
     {
         string[] parts = line.Split(' ');
         HotSprings current = new HotSprings();
         current.map = parts[0];
         current.data = new List<int>();
-        current.done = new List<bool>();
         string[] subparts = parts[1].Split(',');
         for (int i=0; i < subparts.Length; i++)
         {
             current.data.Add(int.Parse(subparts[i]));
-            current.done.Add(false);
         }
+        uint hmask, fmask;
+        hmask = fmask = 0;
+        for (int i=0; i< current.map.Length; i++)
+        {
+            if (current.map[i] == '#') hmask |= (one<<i);
+            if ((current.map[i] == '#') || (current.map[i] == '?')) fmask |= (one<<i);
+        }
+        current.hash_mask = hmask;
+        current.full_mask = fmask;
         hotSprings.Add(current);
     }
     for (int i=0; i<hotSprings.Count; i++)
     {
-        List<int> options = new List<int>();
+        int options = 0;
         HotSprings current = hotSprings[i];
-        string[] valid_bits = current.map.Split('.', StringSplitOptions.RemoveEmptyEntries);
-        if (valid_bits.Length == current.data.Count)
+        int width = current.data.Sum() + current.data.Count - 1;
+        int testwidth = current.map.Length;
+        List<int> startpos = new List<int>();
+        List<int> origstartpos = new List<int>();
+        for (int j = 0; j < current.data.Count; j++)
         {
-            // we have enough sections to data - this should be easy (hahaha)
-            for (int j=0; j<valid_bits.Length; j++)
+            if (j == 0)
             {
-                if (valid_bits[j].Length != current.data[j])
+                startpos.Add(0);
+                origstartpos.Add(0);
+            }
+            else
+            {
+                startpos.Add(startpos[j - 1] + current.data[j - 1] + 1);
+                origstartpos.Add(startpos[j - 1] + current.data[j - 1] + 1);
+            }
+        }
+        int lastpos = testwidth - current.data.Last();
+        bool done = false;
+        while (!done)
+        {
+            uint test = 0;
+            int j;
+            for (j=0; j<current.data.Count; j++)
+            {
+                test += (power2[current.data[j]] << startpos[j]);
+            }
+            if (((test & current.hash_mask) == current.hash_mask) && ((test & current.full_mask) == test))
+            {
+                options++;
+                Console.WriteLine(test);
+            }
+            for (j=0; j<startpos.Count - 1; j++)
+            {
+                if (startpos[j] + 1 + current.data[j] < startpos[j + 1])
                 {
-                    if (current.data[j] > valid_bits[j].Length)
-                    {
-                        Console.WriteLine("We shouldn't get here, I think");
-                    }
-                    else
-                    {
-                        options.Add(valid_bits[j].Length * (valid_bits[j].Length - current.data[j]));
-                    }
+                    startpos[j]++;
+                    break;
                 }
-                if (valid_bits[j].Length == current.data[j])
-                {
-                    current.done[j] = true;
-                    options.Add(1);
-                }
+            }
+            if (j==startpos.Count - 1)
+            {
+                startpos[j]++;
+                if (startpos[j] == lastpos+1) done = true;
+            }
+            for (int k=0; k<j; k++) { startpos[k] = origstartpos[k]; }
+        }
+        Console.WriteLine(options);
+        result += options;
 
-            }
-        }
-        else
-        {
-            int v = 0;
-            for (int j=0; j < current.data.Count; j++)
-            {
-                if (current.data[j] > valid_bits[v].Length)
-                {
-                    Console.WriteLine("We shouldn't get here");
-                }
-            }
-        }
     }
     Console.WriteLine(result);
     Console.ReadLine();
@@ -93,5 +121,6 @@ struct HotSprings
 {
     public string map;
     public List<int> data;
-    public List<bool> done;
+    public uint hash_mask;
+    public uint full_mask;
 };
