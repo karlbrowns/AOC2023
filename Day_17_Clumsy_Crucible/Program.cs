dcc4104f62a9e66fa9890e65de5ed2ff411d6836
karlbrowns <simon.k.brrown@gmail.com>
2023-12-31 15:25:44 +0000

    Day 17 - compiler error and I don't understand.

--- Day_17_Clumsy_Crucible/Program.cs
+++ Day_17_Clumsy_Crucible/Program.cs
@@ -3,107 +3,131 @@ using System.Diagnostics;
 
 List<Nodes> nodes = new List<Nodes>();
 List<List<int>> map = new List<List<int>>();
+List<List<int>> visited_map = new List<List<int>>();
 
+int[,] table = new int[9,4];
 void build_graph(int current_node)
 {
-    if (nodes[current_node].dirn==0)
+    table[1,0] = 1;
+    table[1,1] = 16;
+    table[1,2] = 256;
+    table[2,0] = 2;
+    table[2,1] = 32;
+    table[2,2] = 512;
+    table[4,0] = 4;
+    table[4,1] = 64;
+    table[4,2] = 1024;
+    table[8,0] = 8;
+    table[8,1] = 128;
+    table[8,2] = 2048;
+    current_node = 0;
+    // initial node
+    Nodes temp;
+    temp.x = nodes[current_node].x + 1;
+    temp.y = nodes[current_node].y;
+    temp.dirn = 1;
+    temp.dirsteps = 1;
+    temp.cost = map[temp.y][temp.x];
+    temp.neighbours = new List<int>();
+    nodes.Add(temp);
+    nodes[current_node].neighbours.Add(nodes.Count - 1);
+    temp = new Nodes();
+    temp.x = nodes[current_node].x;
+    temp.y = nodes[current_node].y + 1;
+    temp.dirn = 4;
+    temp.dirsteps = 1;
+    temp.cost = map[temp.y][temp.x];
+    temp.neighbours = new List<int>();
+    nodes.Add(temp);
+    nodes[current_node].neighbours.Add(nodes.Count - 1);
+    bool active_nodes = true;
+    while (active_nodes) 
     {
-        // initial node
-        Nodes temp;
-        temp.x = nodes[current_node].x + 1;
-        temp.y = nodes[current_node].y;
-        temp.dirn = 1;
-        temp.dirsteps = 1;
-        temp.cost = map[temp.y][temp.x];
-        temp.neighbours = new List<int>();
-        nodes.Add(temp);
-        nodes[current_node].neighbours.Add(nodes.Count - 1);
-        temp = new Nodes();
-        temp.x = nodes[current_node].x;
-        temp.y = nodes[current_node].y + 1;
-        temp.dirn = 4;
-        temp.dirsteps = 1;
-        temp.cost = map[temp.y][temp.x];
-        temp.neighbours = new List<int>();
-        nodes.Add(temp);
-        nodes[current_node].neighbours.Add(nodes.Count - 1);
-        build_graph(1);
-        build_graph(2);
-    }
-    else
-    {
-        List<int> dirns = new List<int>();
-        dirns.Add(1); dirns.Add(2); dirns.Add(4); dirns.Add(8);
-        switch (nodes[current_node].dirn)
+        active_nodes = false;
+        for (int thisnode; thisnode < nodes[current_node].neighbours.Count; thisnode++)
         {
-            case 1: dirns.Remove(2); break;
-            case 2: dirns.Remove(1); break;
-            case 4: dirns.Remove(8); break;
-            case 8: dirns.Remove(4); break;
-        }
-        if (nodes[current_node].dirsteps == 3) dirns.Remove(nodes[current_node].dirn);
-
-        foreach (int dirn in dirns)
-        {
-            Nodes temp = new Nodes();
-            bool added = false;
-            switch (dirn)
+            int node = nodes[current_node].neighbours[thisnode];
+            if (visited_map[nodes[node].y][nodes[node].x] == nodes[node].dirn * table[nodes[node].dirn,nodes[node].dirsteps - 1])
             {
-                case 1: // going right
-                    temp.x = nodes[current_node].x + 1;
-                    temp.y = nodes[current_node].y;
-                    if (temp.x >= map[0].Count) break;
-                    temp.dirn = dirn;
-                    if (dirn != nodes[current_node].dirn) temp.dirsteps = 1;
-                    else temp.dirsteps = nodes[current_node].dirsteps + 1;
-                    temp.cost = map[temp.y][temp.x];
-                    temp.neighbours = new List<int>();
-                    nodes.Add(temp);
-                    nodes[current_node].neighbours.Add(nodes.Count - 1);
-                    added = true;
-                    break;
-                case 2: // going left
-                    temp.x = nodes[current_node].x - 1;
-                    temp.y = nodes[current_node].y;
-                    if (temp.x < 0) break;
-                    temp.dirn = dirn;
-                    if (dirn != nodes[current_node].dirn) temp.dirsteps = 1;
-                    else temp.dirsteps = nodes[current_node].dirsteps + 1;
-                    temp.cost = map[temp.y][temp.x];
-                    temp.neighbours = new List<int>();
-                    nodes.Add(temp);
-                    nodes[current_node].neighbours.Add(nodes.Count - 1);
-                    added = true;
-                    break;
-                case 4: // going down
-                    temp.x = nodes[current_node].x;
-                    temp.y = nodes[current_node].y + 1;
-                    if (temp.y >= map.Count) break;
-                    temp.dirn = dirn;
-                    if (dirn != nodes[current_node].dirn) temp.dirsteps = 1;
-                    else temp.dirsteps = nodes[current_node].dirsteps + 1;
-                    temp.cost = map[temp.y][temp.x];
-                    temp.neighbours = new List<int>();
-                    nodes.Add(temp);
-                    nodes[current_node].neighbours.Add(nodes.Count - 1);
-                    added = true;
-                    break;
-                case 8: // going up
-                    temp.x = nodes[current_node].x;
-                    temp.y = nodes[current_node].y - 1;
-                    if (temp.y < 0) break;
-                    temp.dirn = dirn;
-                    if (dirn != nodes[current_node].dirn) temp.dirsteps = 1;
-                    else temp.dirsteps = nodes[current_node].dirsteps + 1;
-                    temp.cost = map[temp.y][temp.x];
-                    temp.neighbours = new List<int>();
-                    nodes.Add(temp);
-                    nodes[current_node].neighbours.Add(nodes.Count - 1);
-                    added = true;
-                    break;
+                nodes[node].dirn = 0;
+                continue;
             }
-            if (added) build_graph(nodes.Count - 1);
+            active_nodes = true;
+            visited_map[nodes[node].y][nodes[node].x] = nodes[node].dirn * table[nodes[node].dirn,nodes[node].dirsteps - 1];
+            List<int> dirns = new List<int>();
+            dirns.Add(1); dirns.Add(2); dirns.Add(4); dirns.Add(8);
+            switch (nodes[node].dirn)
+            {
+                case 1: dirns.Remove(2); break;
+                case 2: dirns.Remove(1); break;
+                case 4: dirns.Remove(8); break;
+                case 8: dirns.Remove(4); break;
+                case 0: dirns = new List<int>(); break;
+            }
+            if (nodes[node].dirsteps == 3) dirns.Remove(nodes[node].dirn);
+
+            foreach (int dirn in dirns)
+            {
+                temp = new Nodes();
+                bool added = false;
+                switch (dirn)
+                {
+                    case 1: // going right
+                        temp.x = nodes[node].x + 1;
+                        temp.y = nodes[node].y;
+                        if (temp.x >= map[0].Count) break;
+                        temp.dirn = dirn;
+                        if (dirn != nodes[node].dirn) temp.dirsteps = 1;
+                        else temp.dirsteps = nodes[node].dirsteps + 1;
+                        temp.cost = map[temp.y][temp.x];
+                        temp.neighbours = new List<int>();
+                        nodes.Add(temp);
+                        nodes[node].neighbours.Add(nodes.Count - 1);
+                        added = true;
+                        break;
+                    case 2: // going left
+                        temp.x = nodes[node].x - 1;
+                        temp.y = nodes[node].y;
+                        if (temp.x < 0) break;
+                        temp.dirn = dirn;
+                        if (dirn != nodes[node].dirn) temp.dirsteps = 1;
+                        else temp.dirsteps = nodes[node].dirsteps + 1;
+                        temp.cost = map[temp.y][temp.x];
+                        temp.neighbours = new List<int>();
+                        nodes.Add(temp);
+                        nodes[node].neighbours.Add(nodes.Count - 1);
+                        added = true;
+                        break;
+                    case 4: // going down
+                        temp.x = nodes[node].x;
+                        temp.y = nodes[node].y + 1;
+                        if (temp.y >= map.Count) break;
+                        temp.dirn = dirn;
+                        if (dirn != nodes[node].dirn) temp.dirsteps = 1;
+                        else temp.dirsteps = nodes[node].dirsteps + 1;
+                        temp.cost = map[temp.y][temp.x];
+                        temp.neighbours = new List<int>();
+                        nodes.Add(temp);
+                        nodes[node].neighbours.Add(nodes.Count - 1);
+                        added = true;
+                        break;
+                    case 8: // going up
+                        temp.x = nodes[node].x;
+                        temp.y = nodes[node].y - 1;
+                        if (temp.y < 0) break;
+                        temp.dirn = dirn;
+                        if (dirn != nodes[node].dirn) temp.dirsteps = 1;
+                        else temp.dirsteps = nodes[node].dirsteps + 1;
+                        temp.cost = map[temp.y][temp.x];
+                        temp.neighbours = new List<int>();
+                        nodes.Add(temp);
+                        nodes[node].neighbours.Add(nodes.Count - 1);
+                        added = true;
+                        break;
+                }
+            }
         }
+        current_node++;
     }
 }
 void P1()
@@ -114,10 +138,12 @@ void P1()
     foreach (string line in System.IO.File.ReadLines(data))
     {
         map.Add(new List<int>());
+        visited_map.Add(new List<int>());
         foreach (char c in line)
         {
             int ch = c - 0x30;
             map[index].Add(ch);
+            visited_map[index].Add(0);
         }
         index++;
     }
