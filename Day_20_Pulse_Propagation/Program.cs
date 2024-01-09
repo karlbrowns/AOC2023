ff97bef17abd423c365d7da6815f32073da558b7
karlbrowns <simon.k.brown@gmail.com>
2024-01-09 13:30:35 +0000

    Day 20 - input parsed

--- Day_20_Pulse_Propagation/Program.cs
+++ Day_20_Pulse_Propagation/Program.cs
@@ -5,9 +5,32 @@ void P1()
 {
     int result = 0;
     int index = 0;
-    String data = "input.txt";
+    String data = "inputtst.txt";
+    char[] delims = { '-', '>', ',' };
+    Dictionary<string, Module> modules = new Dictionary<string, Module>();
+    Module button = new Module("button", 1);
+    modules.Add("button", button);
     foreach (string line in System.IO.File.ReadLines(data))
     {
+        string[] parts = line.Split(delims,StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length > 0)
+        {
+            int type = 0;
+            parts[0] = parts[0].Trim();
+            if (parts[0][0] == '%') type = 3;
+            else if (parts[0][0] == '&') type = 4;
+            else if (string.Compare(parts[0],"broadcaster")==0) type = 2;
+            else type = 5;
+            string name;
+            if ((type == 3) || (type == 4)) name = parts[0].Substring(1);
+            else name = parts[0];
+            Module temp = new Module(name, type);
+            for (int i = 1; i<parts.Length; i++)
+            {
+                temp.outputs.Add(parts[i]);
+            }
+            modules.Add(name, temp);
+        }
     }
     Console.WriteLine(result);
     Console.ReadLine();
@@ -34,3 +57,23 @@ t.Restart();
 P2();
 t.Stop();
 Console.WriteLine("P2 took " + t.ElapsedMilliseconds / 1000.0 + " seconds");
+
+class Module
+{
+    public string name;             // redundant, because it will be in the dictionary, but here for clarity
+    public int type;
+    public int state;
+    public List<string> inputs;
+    public List<string> outputs;
+    public List<int> last_in;
+
+    public Module (string name, int type)
+    {
+        this.name = name;
+        this.type = type;   // 1 = button, 2 = broadcaste, 3 = flip-flop, 4 = and, 5 = output
+        this.inputs = new List<string>();
+        this.outputs = new List<string>();
+        this.last_in = new List<int>();
+        this.state = 0;
+    }
+}
